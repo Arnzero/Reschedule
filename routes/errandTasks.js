d8140dbc7b7dac3fc141634d5fd82e908efef15d
arnzero <afh146@humboldt.edu>
2021-05-25 10:26:51 -0700

    tested all routes, next tackle index pages

--- routes/errandTasks.js
+++ routes/errandTasks.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const MiscTask = require('../models/misc_task')
 const CommTask = require('../models/comm_task')
 
+// All errands route
 router.get('/', (req, res) => {
     res.render('errandTasks/index.ejs')
 })
@@ -12,20 +13,27 @@ router.get('/new', (req, res) => {
     res.render('errandTasks/new.ejs')
 })
     // POST create misc. task
+router.post('/', async (req, res) => {
+    res.send("created errand task!")
+})
     
 // GET show a specific misc. task
-router.get('/show', (req, res) => {
+router.get('/:id', (req, res) => {
     res.render('errandTasks/show.ejs')
 })
 
 // GET a task to edit FORM
-router.get('/edit', (req, res) => {
+router.get('/:id/edit', (req, res) => {
     res.render('errandTasks/edit.ejs')
 })
     // PUT update a task 
-
+router.put('/:id', async (req, res) => {
+    res.send("updated errand task")
+})
 
     //remove a task
-
+router.delete('/:id', async (req, res) => {
+    res.send("deleted errand task")
+})
 
 module.exports = router
--- routes/weekTasks.js
+++ routes/weekTasks.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const MiscTask = require('../models/misc_task')
 const CommTask = require('../models/comm_task')
-
+// don't forget to implement /:id/edit and for 'put', 'DELETE', 'get'
 
 // GET week index -- shows my week
 router.get('/', (req, res) => {
@@ -14,21 +14,27 @@ router.get('/new', (req, res) => {
     res.render('weekTasks/new.ejs')
 })
     // POST create week task
-    
+ router.post('/', async (req, res) => {
+     res.send("Posted new week task!")
+ })   
 // GET view a specific task
-router.get('/show', (req, res) => {
+router.get('/:id', (req, res) => {
     res.render('weekTasks/show.ejs')
 })
 
 // GET a task to edit FORM
-router.get('/edit', (req, res) => {
+router.get('/:id/edit', (req, res) => {
     res.render('weekTasks/edit.ejs')
 })
     // PUT update a task 
-
+router.put('/:id', async (req, res) => {
+    res.send("Updated week task!")
+})
 
     //remove a task
-
+router.delete('/:id', async (req, res) => {
+    res.send("deleted week task!")
+})
 
 
 module.exports = router
